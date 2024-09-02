1ff9b00cb5344a88614f2405a76ac6b999db1d1c
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-09-02 12:20:55 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+ import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner SC = new
+    Scanner(System.in);
+  double numerador = SC.nextInt
+    System.out.println();
+  }
+}
